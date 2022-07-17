3f2563853e568a66906708e6b840a00c7a180c68
Ryan Rendon <Rendon.S.Ryan@gmail.com>
2022-07-17 15:23:07 -0700

    add a route to a form to create a new order and display on the splash page.

--- VendorTracker/Controllers/HomeController.cs
+++ VendorTracker/Controllers/HomeController.cs
@@ -8,7 +8,21 @@ namespace VendorTracker.Controllers
     [Route("/")]
     public ActionResult Index()
     {
+      Order starterOrder = new Order("Add first Order to Vendor history", "", 0, "");
+      return View(starterOrder);
+    }
+
+    [Route("orders/new")]
+    public ActionResult CreateForm()
+    {
       return View();
+    }
+
+    [Route("/orders")]
+    public ActionResult Create(string title, string description, int price, string date)
+    {
+      Order myOrder = new Order(title, description, price, date);
+      return View("Index", myOrder);
     }
 
   }
